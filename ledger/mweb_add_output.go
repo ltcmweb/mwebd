9234de64fb19543c3f485ef2381cf5d8d73a0368
Hector Chu <hectorchu@gmail.com>
2024-08-19 18:30:22 +0100

    Simplify

--- ledger/mweb_add_output.go
+++ ledger/mweb_add_output.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/binary"
 	"io"
-	"math/big"
 
 	secp "github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/ltcmweb/ltcd/ltcutil/mweb"
@@ -28,34 +27,31 @@ func (st *mwebAddOutputState) request(ctx *TxContext) []byte {
 }
 
 func (st *mwebAddOutputState) process(ctx *TxContext, r io.Reader) (txState, error) {
-	result := struct {
-		Commitment        mw.Commitment
-		SenderPubKey      mw.PublicKey
-		ReceiverPubKey    mw.PublicKey
-		Features          wire.MwebOutputMessageFeatureBit
-		KeyExchangePubKey mw.PublicKey
-		ViewTag           byte
-		MaskedValue       uint64
-		MaskedNonce       [16]byte
-		Blind             mw.BlindingFactor
-		Shared            mw.SecretKey
-	}{}
-	err := binary.Read(r, binary.LittleEndian, &result)
-	if err != nil {
+	var (
+		result struct {
+			Commitment     mw.Commitment
+			SenderPubKey   mw.PublicKey
+			ReceiverPubKey mw.PublicKey
+		}
+		result2 struct {
+			Blind  mw.BlindingFactor
+			Shared mw.SecretKey
+		}
+		message wire.MwebOutputMessage
+		msg     bytes.Buffer
+	)
+	if err := binary.Read(r, binary.LittleEndian, &result); err != nil {
 		return nil, err
 	}
-	message := wire.MwebOutputMessage{
-		Features:          result.Features,
-		KeyExchangePubKey: result.KeyExchangePubKey,
-		ViewTag:           result.ViewTag,
-		MaskedValue:       result.MaskedValue,
-		MaskedNonce:       *new(big.Int).SetBytes(result.MaskedNonce[:]),
+	if err := message.Deserialize(io.TeeReader(r, &msg)); err != nil {
+		return nil, err
 	}
-	var msg bytes.Buffer
-	message.Serialize(&msg)
+	if err := binary.Read(r, binary.LittleEndian, &result2); err != nil {
+		return nil, err
+	}
 	recipient := ctx.Recipients[st.index]
 	rangeProof := secp256k1.NewRangeProof(recipient.Value,
-		*mw.BlindSwitch(&result.Blind, recipient.Value),
+		*mw.BlindSwitch(&result2.Blind, recipient.Value),
 		make([]byte, 20), msg.Bytes())
 	ctx.outputs = append(ctx.outputs, &wire.MwebOutput{
 		Commitment:     result.Commitment,
@@ -66,10 +62,10 @@ func (st *mwebAddOutputState) process(ctx *TxContext, r io.Reader) (txState, err
 		RangeProofHash: blake3.Sum256(rangeProof[:]),
 	})
 	ctx.NewCoins = append(ctx.NewCoins, &mweb.Coin{
-		Blind:        &result.Blind,
+		Blind:        &result2.Blind,
 		Value:        recipient.Value,
 		Address:      recipient.Address,
-		SharedSecret: &result.Shared,
+		SharedSecret: &result2.Shared,
 	})
 	return &mwebSignOutputState{st.index}, nil
 }
